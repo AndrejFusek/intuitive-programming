c7b37abc33164fabd57d478de7749a542b07f8af
Andrej Fusek <andrej.fusek@gmail.com>
2022-04-05 10:38:20 +0200

    Add S01E02 Example

---
+++ S01E02/S01E02/S01E02.cpp
@@ -0,0 +1,44 @@
+﻿// S01E02.cpp : Defines the entry point for the application.
+//
+
+#include "S01E02.h"
+
+
+int read_number(std::istream& stream)
+{
+	int number;
+	stream >> number;
+	return number;
+}
+
+char read_character(std::istream& stream)
+{
+	char ch;
+	stream >> ch;
+	return ch;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+void print(int number)
+{
+	std::cout << number << std::endl;
+}
+
+
+int main()
+{
+	std::istream &stream = std::cin;
+	int a = read_number(stream);
+	char operation = read_character(stream);
+	int b = read_number(stream);
+
+	int result = add(a, b);
+
+	print(result);
+
+	return 0;
+}
